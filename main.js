dd94bd224cdce7ba7efd12da6d86f749516f7a50
Nursultan24 <Nursultanmustafaev15@gmail.com>
2024-03-09 16:33:14 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Напишите функцию, которая принимает массив булевых значений и возвращает массив только со значениями true
+// Input: [true,false,true,true]
+// Output: [true,true,true]
+// Input: [false,true]	
+// Output: [true]
+
+// function filterTrue(item){
+//     return item.filter(value => value === true )
+// }
+// console.log(filterTrue([false,true]));
